855642b47256b8d10e5e4f6438f65304ad9cc565
tvigneron <47259973+tvigneron@users.noreply.github.com>
2020-11-20 10:42:06 -0800

    Update enable_maclearning.py
    Updating to map more to other scripts in the document

--- enable_maclearning.py
+++ enable_maclearning.py
@@ -2,23 +2,44 @@
 # Disclaimer: This product is not supported by VMware.
 # License: https://github.com/vmware/pyvmomi-community-samples/blob/master/LICENSE
 
-# Purpose:  Enables MAC Learning and Forged Transmit on a given PortGroup 
+# Purpose:  Enables MAC Learning and Forged Transmit on a given PortGroup
 
 
 from pyVim.connect import SmartConnect
 import ssl
 from pyVmomi import vim
 import time
+import argparse
+
 
 #Put relevant informations for your environment:
-vcenter_ip = "VCENTER IP/FQDN"
-vcenter_user = "USER"
-vcenter_password = "VCENTER PASSWORD"
-portgroup_name = "PORTGROUP NAME"
+def parseParameters():
+    parser = argparse.ArgumentParser(
+            description='Arguments to connect to vCenter')
+    parser.add_argument('-s', '--sourcevc',
+            required = True,
+            action = 'store',
+            help = 'Vcenter server name or IP')
 
-def connect_vcenter(vcenter_ip, vcenter_user, vcenter_password):
+    parser.add_argument('-u', '--user',
+            required=True,
+            action='store',
+            help='User name to connect to vcenter')
+    parser.add_argument('-p', '--password',
+            required=True,
+            action='store',
+            help = 'Password for connection to vcenter')
+
+    parser.add_argument('-pg', '--portgroup',
+                        required=True,
+                        action='store',
+                        help= 'PortGroup (name) on which to set MAC Learning and Forged Transmit')
+    args = parser.parse_args()
+    return args
+
+def connect_vcenter(sourcevc, user, password):
     context = ssl._create_unverified_context()
-    si= SmartConnect(host=str(vcenter_ip), user=str(vcenter_user), pwd=vcenter_password,sslContext=context)
+    si= SmartConnect(host=str(sourcevc), user=str(user), pwd=password,sslContext=context)
     return si
 
 def get_all_objs(content, vimtype):
@@ -33,28 +54,24 @@ def wait_for_task(task, actionName='job', hideResult=False):
         time.sleep(2)
 
     if task.info.state == vim.TaskInfo.State.success:
-        if task.info.result is not None and not hideResult:
-            output = '%s completed successfully, result: %s' % (actionName, task.info.result)
-            print(output)
-        else:
-            output = '%s completed successfully.' % actionName
-            print(output)
+        output = 'Success.\n The task %s completed successfully.' % actionName
+        print(output)
     else:
-        output = '%s failed and did not complete successfully: %s' % (actionName, task.info.error)
+        output = 'The task %s failed and did not complete successfully: %s' % (actionName, task.info.error)
         raise task.info.error
         print(output)
 
     return task.info.result
 
-def enable_maclearning_forgedtransmit(si, content, portgroup_name):
-    for portgroup in get_all_objs(content, [vim.dvs.DistributedVirtualPortgroup]):
-        if portgroup.name == portgroup_name:
+def enable_maclearning_forgedtransmit(si, content, portgroup):
+    for virtualportgroup in get_all_objs(content, [vim.dvs.DistributedVirtualPortgroup]):
+        if virtualportgroup.name == portgroup:
             spec = vim.dvs.DistributedVirtualPortgroup.ConfigSpec()
             spec.defaultPortConfig = vim.dvs.VmwareDistributedVirtualSwitch.VmwarePortConfigPolicy()
-            spec.configVersion = portgroup.config.configVersion
+            spec.configVersion = virtualportgroup.config.configVersion
 
             spec.defaultPortConfig.macManagementPolicy= vim.dvs.VmwareDistributedVirtualSwitch.MacManagementPolicy()
-            spec.defaultPortConfig.macManagementPolicy.macLearningPolicy= portgroup.config.defaultPortConfig.macManagementPolicy.macLearningPolicy
+            spec.defaultPortConfig.macManagementPolicy.macLearningPolicy= virtualportgroup.config.defaultPortConfig.macManagementPolicy.macLearningPolicy
             spec.defaultPortConfig.macManagementPolicy.inherited = False
 
             spec.defaultPortConfig.macManagementPolicy.macLearningPolicy.enabled = True
@@ -64,13 +81,16 @@ def enable_maclearning_forgedtransmit(si, content, portgroup_name):
 
             spec.defaultPortConfig.macManagementPolicy.forgedTransmits = True
 
-            task = portgroup.ReconfigureDVPortgroup_Task(spec = spec)
+            task = virtualportgroup.ReconfigureDVPortgroup_Task(spec = spec)
             wait_for_task(task, si)
 
 
 if __name__ == "__main__":
+    #Get user input from CLI
+    args = parseParameters()
+    print(args.sourcevc, args.user, args.password, args.portgroup)
     #Initiate a session to vCenter
-    si = connect_vcenter(vcenter_ip, vcenter_user, vcenter_password)
+    si = connect_vcenter(args.sourcevc, args.user, args.password)
     content = si.content
     #Enable Mac learning and forged transmit
-    enable_maclearning_forgedtransmit(si, content, portgroup_name)
+    enable_maclearning_forgedtransmit(si, content, args.portgroup)
